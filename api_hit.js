bd20d71da3ce8bdec3e461055c94afc96ad939d8
Zachary Landes <zach.g.landes@gmail.com>
2015-11-07 13:48:43 +1300

    jsonerror

---
+++ api_hit.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+path.join(__dirname, '.naturehack/node_modules')
+
+ var newzealandorganismsregisterlib = require('newzealandorganismsregisterlib');
+
+
+   var config = newzealandorganismsregisterlib.configuration;
+     config.apikey = a_secret_key;
+
+
+  var controller = newzealandorganismsregisterlib.MatchesController;
+        controller.getItems(id, callback);
+
+        console.log("you")
+
+
---
+++ app.js
@@ -0,0 +1,1 @@
+var nzor = require('newzealandorganismsregisterlib')
