8fe7986bfac3efe9b8bb612cbc5a2c4d7af5cc34
Zachary Landes <zach.g.landes@gmail.com>
2015-11-07 22:20:49 +1300

    moresheet

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var nzor = require('newzealandorganismsregisterlib')
 var names = nzor.NamesController
+var matches = nzor.MatchesController
+var stats = nzor.StatisticsController
 
-names.getNamesGetSearch({query: "Aves"}, function(err,res){
+names.getNamesGet({parentNameId: '000423b0-7771-4117-a33c-9075a6a6fa02'}, function(err,res){
 	if(!err){
-		console.log("boo")
+    for (var i =0;i<=9;i++){
+		console.log(res.names[i].fullName)}
 	}
 })
+// stats.getStatisticsGetByName('aves', function(err,res){
+//   if(!err){
+//     console.log(res)
+//   }
+// })
+
+// names.getNamesGetByID('000423b0-7771-4117-a33c-9075a6a6fa02', function(err,res){
+//   if(!err){
+//     console.log(res)
+//   }
+// })
+
