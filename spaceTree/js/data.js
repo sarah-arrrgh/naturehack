3fdd8779dafc10bd69ebfb89f61932f0f52a7026
[01;31m[K"name"[m[K: "Sarah Rogers <[01;31m[K"email"[m[K: "srogersnz@gmail.com>
2015-11-08 02:40:27 +1300

    Add more JSON

--- spaceTree/js/data.js
+++ spaceTree/js/data.js
@@ -29,84 +29,324 @@ var dataInput = {
                     data: {},
                     children: []
                     }]
-                }, {
-                id: "0112",
-                name: "0112",
-                data: {},
-                children: [{
-                    id: "01121",
-                    name: "01121",
-                    data: {},
-                    children: []
-                }, {
-                    id: "01122",
-                    name: "01122",
-                    data: {},
-                    children: []
-                }, {
-                    id: "01123",
-                    name: "01123",
-                    data: {},
-                    children: []
-                }, {
-                    id: "01124",
-                    name: "01124",
-                    data: {},
-                    children: []
-                }]
-            }, {
-                id: "0113",
-                name: "0113",
-                data: {},
-                children: [{
-                    id: "01131",
-                    name: "01131",
-                    data: {},
-                    children: []
                 }]
             }]
         }, {
-            id: "012",
-            name: "012",
-            data: {},
-            children: [{
-                id: "0121",
-                name: "0121",
-                data: {},
-                children: [{
-                    id: "01211",
-                    name: "01211",
-                    data: {},
-                    children: []
-                }]
-            }]
-        }]
-    }, {
         id: "02",
-        name: "Archaeognatha",
+        name: "Dicondylia",
         data: {},
         children: [{
             id: "021",
-            name: "021",
+            name: "Pterygota",
             data: {},
             children: [{
                 id: "0211",
-                name: "0211",
+                name: "Ephemeroptera",
                 data: {},
                 children: [{
                     id: "02111",
-                    name: "02111",
+                    name: "Ameletopsidae",
+                    data: {},
+                    children: [{
+                        id: "021111",
+                        name: "Ameletopsis perscitus (Eaton, 1899)",
+                        data: {},
+                        children: []
+                    }]
+                }, {
+                    id: "02112",
+                    name: "Coloburiscidae",
+                    data: {},
+                    children: [{
+                        id: "021121",
+                        name: "Coloburiscus",
+                        data: {},
+                        children: [{
+                            id: "0211211",
+                            name: "Coloburiscus humeralis (Walker, 1853)",
+                            data: {},
+                            children: []
+                        }]
+                    }]
+                }, {
+                    id: "02113",
+                    name: "Ephemeridae",
+                    data: {},
+                    children: [{
+                        id: "021131",
+                        name: "Ichthybotus",
+                        data: {},
+                        children: [{
+                            id: "0211311",
+                            name: "Ichthybotus bicolor Tillyard, 1923",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "0211311",
+                            name: "Ichthybotus hudsoni (McLachlan, 1894)",
+                            data: {},
+                            children: []
+                        }]
+                    }]
+                }, {
+                    id: "02114",
+                    name: "Leptophlebiidae",
+                    data: {},
+                    children: [{
+                        id: "0211401",
+                        name: "Acanthophlebia",
+                        data: {},
+                        children: [{
+                            id: "02114011",
+                            name: "Acanthophlebia cruentata (Hudson, 1904)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211402",
+                        name: "Arachnocolus",
+                        data: {},
+                        children: [{
+                            id: "02114021",
+                            name: "Arachnocolus phillipsi Towns & Peters, 1979",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211403",
+                        name: "Atalophlebioides",
+                        data: {},
+                        children: [{
+                            id: "02114031",
+                            name: "Atalophlebioides cromwelli (Phillips, 1930)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211404",
+                        name: "Aupouriella",
+                        data: {},
+                        children: [{
+                            id: "02114041",
+                            name: "Aupouriella pohei Winterbourn, 2009",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211405",
+                        name: "Austroclima",
+                        data: {},
+                        children: [{
+                            id: "02114051",
+                            name: "Austroclima jollyae Towns & Peters, 1979",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114052",
+                            name: "Austroclima sepia (Phillips, 1930)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211406",
+                        name: "Austronella",
+                        data: {},
+                        children: [{
+                            id: "02114061",
+                            name: "Austronella planulata (Towns, 1983)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211407",
+                        name: "Cryophlebia",
+                        data: {},
+                        children: [{
+                            id: "02114071",
+                            name: "Cryophlebia aucklandensis (Peters, 1971)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211408",
+                        name: "Deleatidium",
+                        data: {},
+                        children: [{
+                            id: "021140801",
+                            name: "Deleatidium (Deleatidium) angustum Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140802",
+                            name: "Deleatidium (Deleatidium) atricolor Hitchings, 2009",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140803",
+                            name: "Deleatidium (Deleatidium) autumnale Phillips, 1930",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140804",
+                            name: "Deleatidium (Deleatidium) branchiola Hitchings, 2009",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140805",
+                            name: "Deleatidium (Deleatidium) cerinum Phillips, 1930",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140806",
+                            name: "Deleatidium (Deleatidium) fumosum Phillips, 1930",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140807",
+                            name: "Deleatidium (Deleatidium) lillii Eaton, 1899",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140808",
+                            name: "Deleatidium (Deleatidium) magnum Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140809",
+                            name: "Deleatidium (Deleatidium) myzobranchia Phillips, 1930",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140810",
+                            name: "Deleatidium (Deleatidium) n. sp.",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140811",
+                            name: "Deleatidium (Deleatidium) townsi Hitchings, 2009",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "021140812",
+                            name: "Deleatidium (Deleatidium) vernale Phillips, 1930",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211409",
+                        name: "Isothraulus",
+                        data: {},
+                        children: [{
+                            id: "02114091",
+                            name: "Isothraulus abditus Towns & Peters, 1979",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211410",
+                        name: "Mauiulus",
+                        data: {},
+                        children: [{
+                            id: "02114101",
+                            name: "Mauiulus aquilus Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114102",
+                            name: "Mauiulus luma Towns & Peters, 1979",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211411",
+                        name: "Neozephlebia",
+                        data: {},
+                        children: [{
+                            id: "02114111",
+                            name: "Neozephlebia scita (Walker, 1853)",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211412",
+                        name: "Tepakia",
+                        data: {},
+                        children: [{
+                            id: "02114121",
+                            name: "Tepakia caligata Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }]
+                    }, {
+                        id: "0211413",
+                        name: "Zephlebia",
+                        data: {},
+                        children: [{
+                            id: "02114131",
+                            name: "Zephlebia borealis (Phillips, 1930)",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114132",
+                            name: "Zephlebia dentata (Eaton, 1871)",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114133",
+                            name: "Zephlebia inconspicua Towns, 1983",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114134",
+                            name: "Zephlebia nebulosa Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114135",
+                            name: "Zephlebia pirongia Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114136",
+                            name: "Zephlebia spectabilis Towns, 1983",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114137",
+                            name: "Zephlebia tuberculata Towns & Peters, 1996",
+                            data: {},
+                            children: []
+                        }, {
+                            id: "02114138",
+                            name: "Zephlebia versicolor (Eaton, 1899)",
+                            data: {},
+                            children: []
+                        }]
+                    }]
+                }, {
+                    id: "02115",
+                    name: "Nesameletidae",
                     data: {},
                     children: []
                 }, {
-                    id: "02112",
-                    name: "02112",
+                    id: "02116",
+                    name: "Oniscigastridae",
+                    data: {},
+                    children: []
+                }, {
+                    id: "02117",
+                    name: "Rallidentidae",
+                    data: {},
+                    children: []
+                }, {
+                    id: "02118",
+                    name: "Siphlaenigmatidae",
                     data: {},
                     children: []
                 }]
             }, {
                 id: "0212",
-                name: "0212",
+                name: "Neoptera",
                 data: {},
                 children: [{
                     id: "02121",
@@ -114,10 +354,20 @@ var dataInput = {
                     data: {},
                     children: []
                 }]
+            }, {
+                id: "0213",
+                name: "Odonata",
+                data: {},
+                children: [{
+                    id: "02131",
+                    name: "02131",
+                    data: {},
+                    children: []
+                }]
             }]
         }, {
             id: "022",
-            name: "022",
+            name: "Thysanura",
             data: {},
             children: [{
                 id: "0221",
@@ -134,151 +384,6 @@ var dataInput = {
                     data: {},
                     children: []
                 }]
-            }]
-        }, {
-            id: "023",
-            name: "023",
-            data: {},
-            children: [{
-                id: "0231",
-                name: "0231",
-                data: {},
-                children: [{
-                    id: "02311",
-                    name: "02311",
-                    data: {},
-                    children: []
-                }, {
-                    id: "02312",
-                    name: "02312",
-                    data: {},
-                    children: []
-                }]
-            }]
-        }]
-    }, {
-        id: "03",
-        name: "Dicondylia",
-        data: {},
-        children: [{
-            id: "031",
-            name: "031",
-            data: {},
-            children: [{
-                id: "0311",
-                name: "0311",
-                data: {},
-                children: [{
-                    id: "03111",
-                    name: "03111",
-                    data: {},
-                    children: []
-                }, {
-                    id: "03112",
-                    name: "03112",
-                    data: {},
-                    children: []
-                }, {
-                    id: "03113",
-                    name: "03113",
-                    data: {},
-                    children: []
-                }]
-            }, {
-                id: "0312",
-                name: "0312",
-                data: {},
-                children: []
-            }, {
-                id: "0313",
-                name: "0313",
-                data: {},
-                children: []
-            }, {
-                id: "0314",
-                name: "0314",
-                data: {},
-                children: [{
-                    id: "03141",
-                    name: "03141",
-                    data: {},
-                    children: []
-                }, {
-                    id: "03143",
-                    name: "03142",
-                    data: {},
-                    children: []
-                }]
-            }]
-        }, {
-            id: "032",
-            name: "032",
-            data: {},
-            children: []
-        }, {
-            id: "033",
-            name: "033",
-            data: {},
-            children: []
-        }, {
-            id: "034",
-            name: "034",
-            data: {},
-            children: []
-        }, {
-            id: "035",
-            name: "035",
-            data: {},
-            children: []
-        }]
-    }, {
-        id: "04",
-        name: "Dicondylia",
-        data: {},
-        children: [{
-            id: "041",
-            name: "041",
-            data: {},
-            children: [{
-                id: "0411",
-                name: "0411",
-                data: {},
-                children: [{
-                    id: "04111",
-                    name: "04111",
-                    data: {},
-                    children: []
-                }, {
-                    id: "04112",
-                    name: "04112",
-                    data: {},
-                    children: []
-                }, {
-                    id: "04113",
-                    name: "04113",
-                    data: {},
-                    children: []
-                }, {
-                    id: "04114",
-                    name: "04114",
-                    data: {},
-                    children: []
-                }, {
-                    id: "04115",
-                    name: "04115",
-                    data: {},
-                    children: []
-                }]
-            }]
-        }, {
-            id: "042",
-            name: "042",
-            data: {},
-            children: [{
-                id: "0421",
-                name: "0421",
-                data: {},
-                children: []
             }]
         }]
     }]
