5928e037b364e672705b7bb14b2bcd54fd257c82
[01;31m[K"name"[m[K: "Flinn Gendall", &   [01;31m[K"name"[m[K: "Sarah Rogers <pair+flinnja.sarah-arrrgh@devbootcamp.com>
2015-11-07 18:14:57 +1300

    Hardcode JSON tree with numerical tree structure.

--- spaceTree/example.js
+++ spaceTree/example.js
@@ -1,4 +1,4 @@
-var labelType, useGradients, nativeTextSupport, animate;
+var labelType, useGradients, nativeTextSupport, animate
 
 (function() {
   var ua = navigator.userAgent,
@@ -6,24 +6,24 @@ var labelType, useGradients, nativeTextSupport, animate;
       typeOfCanvas = typeof HTMLCanvasElement,
       nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
       textSupport = nativeCanvasSupport
-        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function')
   //I'm setting this based on the fact that ExCanvas provides text support for IE
   //and that as of today iPhone/iPad current text support is lame
-  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
-  nativeTextSupport = labelType == 'Native';
-  useGradients = nativeCanvasSupport;
-  animate = !(iStuff || !nativeCanvasSupport);
-})();
+  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML'
+  nativeTextSupport = labelType == 'Native'
+  useGradients = nativeCanvasSupport
+  animate = !(iStuff || !nativeCanvasSupport)
+})()
 
 var Log = {
   elem: false,
   write: function(text){
     if (!this.elem)
-      this.elem = document.getElementById('log');
-    this.elem.innerHTML = text;
-    this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+      this.elem = document.getElementById('log')
+    this.elem.innerHTML = text
+    this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px'
   }
-};
+}
 
 
 function init(){
@@ -303,15 +303,15 @@ function init(){
                 }]
             }]
         }]
-    };
+    }
     //end
     //init Spacetree
     //Create a new ST instance
-    var st = new $jit.ST({
+    var ruru = new $jit.ST({
         //id of viz container element
         injectInto: 'infovis',
         //set duration for the animation
-        duration: 800,
+        duration: 500,
         //set animation transition type
         transition: $jit.Trans.Quart.easeInOut,
         //set distance between node and its children
@@ -325,7 +325,7 @@ function init(){
         //set overridable=true for styling individual
         //nodes or edges
         Node: {
-            height: 20,
+            height: 30,
             width: 60,
             type: 'rectangle',
             color: '#aaa',
@@ -338,35 +338,35 @@ function init(){
         },
 
         onBeforeCompute: function(node){
-            Log.write("loading " + node.name);
+            Log.write("loading " + node.name)
         },
 
         onAfterCompute: function(){
-            Log.write("done");
+            Log.write("done")
         },
 
         //This method is called on DOM label creation.
         //Use this method to add event handlers and styles to
         //your node.
         onCreateLabel: function(label, node){
-            label.id = node.id;
-            label.innerHTML = node.name;
+            label.id = node.id
+            label.innerHTML = node.name
             label.onclick = function(){
               if(normal.checked) {
-                st.onClick(node.id);
+                ruru.onClick(node.id)
               } else {
-                st.setRoot(node.id, 'animate');
+                ruru.setRoot(node.id, 'animate')
               }
-            };
+            }
             //set label styles
-            var style = label.style;
-            style.width = 60 + 'px';
-            style.height = 17 + 'px';
-            style.cursor = 'pointer';
-            style.color = '#333';
-            style.fontSize = '0.8em';
-            style.textAlign= 'center';
-            style.paddingTop = '3px';
+            var style = label.style
+            style.width = 60 + 'px'
+            style.height = 17 + 'px'
+            style.cursor = 'pointer'
+            style.color = '#333'
+            style.fontSize = '0.8em'
+            style.textAlign = 'center'
+            style.paddingTop = '3px'
         },
 
         //This method is called right before plotting
@@ -378,18 +378,18 @@ function init(){
             //add some color to the nodes in the path between the
             //root node and the selected node.
             if (node.selected) {
-                node.data.$color = "#ff7";
+                node.data.$color = "#ff7"
             }
             else {
-                delete node.data.$color;
+                delete node.data.$color
                 //if the node belongs to the last plotted level
                 if(!node.anySubnode("exist")) {
                     //count children number
-                    var count = 0;
-                    node.eachSubnode(function(n) { count++; });
+                    var count = 0
+                    node.eachSubnode(function(n) { count++ })
                     //assign a node color based on
                     //how many children it has
-                    node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count];
+                    node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count]
                 }
             }
         },
@@ -401,44 +401,44 @@ function init(){
         //override the Edge global style properties.
         onBeforePlotLine: function(adj){
             if (adj.nodeFrom.selected && adj.nodeTo.selected) {
-                adj.data.$color = "#eed";
-                adj.data.$lineWidth = 3;
+                adj.data.$color = "#eed"
+                adj.data.$lineWidth = 3
             }
             else {
-                delete adj.data.$color;
-                delete adj.data.$lineWidth;
+                delete adj.data.$color
+                delete adj.data.$lineWidth
             }
         }
-    });
+    })
     //load json data
-    st.loadJSON(json);
+    ruru.loadJSON(json)
     //compute node positions and layout
-    st.compute();
+    ruru.compute()
     //optional: make a translation of the tree
-    st.geom.translate(new $jit.Complex(-200, 0), "current");
+    ruru.geom.translate(new $jit.Complex(-200, 0), "current")
     //emulate a click on the root node.
-    st.onClick(st.root);
+    ruru.onClick(ruru.root)
     //end
     //Add event handlers to switch spacetree orientation.
     var top = $jit.id('r-top'),
         left = $jit.id('r-left'),
         bottom = $jit.id('r-bottom'),
         right = $jit.id('r-right'),
-        normal = $jit.id('s-normal');
+        normal = $jit.id('s-normal')
 
 
     function changeHandler() {
         if(this.checked) {
-            top.disabled = bottom.disabled = right.disabled = left.disabled = true;
-            st.switchPosition(this.value, "animate", {
+            top.disabled = bottom.disabled = right.disabled = left.disabled = true
+            ruru.switchPosition(this.value, "animate", {
                 onComplete: function(){
-                    top.disabled = bottom.disabled = right.disabled = left.disabled = false;
+                    top.disabled = bottom.disabled = right.disabled = left.disabled = false
                 }
-            });
+            })
         }
-    };
+    }
 
-    top.onchange = left.onchange = bottom.onchange = right.onchange = changeHandler;
+    top.onchange = left.onchange = bottom.onchange = right.onchange = changeHandler
     //end
 
 }
