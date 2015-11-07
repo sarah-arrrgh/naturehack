5c24b4a827239cf4f38c60d747046922cde563f0
Zachary Landes <zach.g.landes@gmail.com>
2015-11-08 02:40:24 +1300

    argh

--- spaceTree/js/example.js
+++ spaceTree/js/example.js
@@ -27,6 +27,13 @@ var Log = {
 
 
 function init(){
+      function getRandomColor(brightness){
+    //6 levels of brightness from 0 to 5, 0 being the darkest
+    var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+    var mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
+    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){ return Math.round(x/2.0)})
+    return "rgb(" + mixedrgb.join(",") + ")";
+  }
     //init data
     var json = dataInput
     //end
@@ -52,7 +59,7 @@ function init(){
         Node: {
           overridable: true,
           type: 'circle',
-          color: '#3a3',
+          color: getRandomColor(2),
           alpha: 1,
           dim: 70,
           autoHeight: false,
@@ -109,7 +116,7 @@ function init(){
             //add some color to the nodes in the path between the
             //root node and the selected node.
             if (node.selected) {
-                node.data.$color = "#ff7"
+                node.data.$color = getRandomColor(2)
                 node.data.$dim = 120
             }
             else {
